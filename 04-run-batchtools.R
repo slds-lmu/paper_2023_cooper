a19c778a6af067052c96172a34239002a4d5c092
Lukas Burk <lukas@quantenbrot.de>
2022-03-21 16:48:25 +0100

    preliminary batchtools/analysis scripts

---
+++ 04-run-batchtools.R
@@ -0,0 +1,83 @@
+library(batchtools)
+
+# settings ----------------------------------------------------------------
+config <- list(
+  global_seed = 563,
+  sim_seed = 569,
+  sim_n = 1000,
+  sim_cache = TRUE,
+  repls = 100
+)
+
+set.seed(config$global_seed)
+
+# Registry ----------------------------------------------------------------
+if (!file.exists(here::here("registries"))) dir.create(here::here("registries"))
+reg_name <- "fwel_simulations"
+reg_dir <- here::here("registries", reg_name)
+unlink(reg_dir, recursive = TRUE)
+makeExperimentRegistry(file.dir = reg_dir)
+
+# Problems -----------------------------------------------------------
+source("02-problems.R")
+addProblem(name = "sim_a", fun = sim_a, seed = config$sim_seed, cache = config$sim_cache)
+addProblem(name = "sim_b", fun = sim_b, seed = config$sim_seed, cache = config$sim_cache)
+addProblem(name = "sim_c", fun = sim_c, seed = config$sim_seed, cache = config$sim_cache)
+addProblem(name = "sim_d", fun = sim_d, seed = config$sim_seed, cache = config$sim_cache)
+
+# Algorithms -----------------------------------------------------------
+source("03-algorithms.R")
+addAlgorithm(name = "fwel_mt", fun = fwel_mt_wrapper)
+
+
+# Experiments -----------------------------------------------------------
+prob_design <- list(
+  sim_a = expand.grid(n = config$sim_n),
+  sim_b = expand.grid(n = config$sim_n),
+  sim_c = expand.grid(n = config$sim_n),
+  sim_d = expand.grid(n = config$sim_n)
+)
+
+algo_design <- list(
+  fwel_mt = expand.grid(
+    mt_max_iter = 2,
+    alpha = 1,
+    z_scale = c(1, 10, 100),
+    z_method = c("original", "aligned")
+  )
+)
+
+addExperiments(prob_design, algo_design, repls = config$repls)
+summarizeExperiments()
+unwrap(getJobPars(), c("algo.pars", "prob.pars"))
+
+# Test jobs -----------------------------------------------------------
+testJob(id = 1)
+
+# Submit -----------------------------------------------------------
+if (grepl("node\\d{2}|bipscluster", system("hostname", intern = TRUE))) {
+  ids <- findNotStarted()
+  ids[, chunk := chunk(job.id, chunk.size = 50)]
+  submitJobs(ids = ids, # walltime in seconds, 10 days max, memory in MB
+             resources = list(name = reg_name, chunks.as.arrayjobs = TRUE,
+                              ncpus = 1, memory = 6000, walltime = 10*24*3600,
+                              max.concurrent.jobs = 40))
+} else {
+  ids <- findNotStarted()
+  ids <- sample(ids$job.id, 250)
+  submitJobs(ids = ids)
+}
+waitForJobs()
+
+
+# Monitor jobs ------------------------------------------------------------
+
+nrow(findRunning())
+nrow(findExpired())
+nrow(findDone())
+nrow(findNotDone())
+
+# Get results -------------------------------------------------------------
+res <-  ijoin(reduceResultsDataTable(), flatten(getJobPars()))
+res
+
