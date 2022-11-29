b8e5e547f0dd70b3a4122830c925f51c755109e2
Lukas Burk <burk@leibniz-bips.de>
2022-11-29 01:02:15 +0100

    Fix monitor

--- monitor.R
+++ monitor.R
@@ -13,16 +13,16 @@ cat("\n")
 
 tbl_running <- unwrap(getJobTable(findRunning()))
 if (nrow(tbl_running) > 0) {
-  tbl_running[, c("job.id", "time.running", "thresh")]
-  tbl_running[, .(count = .N), by = thresh]
+  tbl_running[, c("job.id", "time.running", "algorithm")]
+  tbl_running[, .(count = .N), by = algorithm]
 }
 
 # Done --------------------------------------------------------------------
 cli::cli_h1("Done")
 tbl_done <- unwrap(getJobTable(findDone()))
 if (nrow(tbl_done) > 0) {
-  tbl_done <- tbl_done[, c("job.id", "time.running", "thresh")]
-  tbl_done[, .(count = .N), by = thresh]
+  tbl_done <- tbl_done[, c("job.id", "time.running", "algorithm")]
+  tbl_done[, .(count = .N), by = algorithm]
 }
 cat("\n")
 
@@ -30,8 +30,8 @@ cat("\n")
 cli::cli_h1("Expired")
 tbl_expired <- unwrap(getJobTable(findExpired()))
 if (nrow(tbl_expired) > 0) {
-  tbl_expired <- tbl_expired[, c("job.id", "time.running", "thresh")]
-  tbl_expired[, .(count = .N), by = thresh]
+  tbl_expired <- tbl_expired[, c("job.id", "time.running", "algorithm")]
+  tbl_expired[, .(count = .N), by = algorithm]
 }
 cat("\n")
 
