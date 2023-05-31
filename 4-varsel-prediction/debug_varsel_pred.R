438ae956106f4598ad9c1608b05e6f2c1d8c6c97
Lukas Burk <lukas@quantenbrot.de>
2023-05-31 16:34:25 +0200

    Debug

--- 4-varsel-prediction/debug_varsel_pred.R
+++ 4-varsel-prediction/debug_varsel_pred.R
@@ -8,7 +8,13 @@ library(randomForestSRC)
 library(survival)
 library(rlang)
 
-instance <- sim_surv_binder(n_train = 400, n_test = 200, p = 5000, ce = 0.5, lambda = 0.1, lambda_c = 0.1)
+# instance <- sim_surv_binder(n_train = 400, n_test = 200, p = 5000, ce = 0.25, lambda1 = 0.1, lambda2 = 0.1, lambda_c = 0.1)
+# table(instance$train$status) |> (\(x) {print(x); cat("\n"); x})() |> as.numeric() |> summary()
+
+instance <- get_bladder_data(type = "geno", split = 2/3)
+table(instance$train$status) |> (\(x) {print(x); cat("\n"); x})() |> as.numeric() |> summary()
+
+min(table(instance$train$status)) / 20
 
 # xdf <- instance$data
 # xdf$status <- factor(xdf$status)
@@ -18,18 +24,21 @@ instance <- sim_surv_binder(n_train = 400, n_test = 200, p = 5000, ce = 0.5, lam
 tictoc::tic()
 mt_max_iter <- 3
 fit <- fwelnet::fwelnet_mt_cox(
-  instance$train,
+  as.data.frame(instance$train),
   mt_max_iter = mt_max_iter,
   alpha = 1,
   t = 100,
   a = 0.5,
+  stratify_by_status = TRUE,
+  nfolds = 5,
   thresh = 1e-3,
   include_mt_beta_history = TRUE
 )
 
 tictoc::toc()
 
-saveRDS(object = fit, file = here::here("fwel-testfit.rds"))
+#saveRDS(object = fit, file = here::here("fwel-testfit.rds"))
+saveRDS(object = fit, file = here::here("fwel-testfit-bladder.rds"))
 
 
 p <- ncol(instance$train)
@@ -52,13 +61,22 @@ fw_coefs <- list(
   )
 )
 
+setdiff(names(fw_coefs$fwelnet$cause1), names(fw_coefs$glmnet$cause1))
+setequal(names(fw_coefs$fwelnet$cause1), names(fw_coefs$glmnet$cause1))
+
+if (setequal(names(fw_coefs$fwelnet$cause1), names(fw_coefs$glmnet$cause1))) {
+  print(names(fw_coefs$fwelnet$cause1))
+  stop("fwelnet and glmnet have selected identical variables")
+}
+
 
 # from fit_csc
 library(data.table)
+library(riskRegression)
 train <- as.data.table(instance$train)
 test <- as.data.table(instance$test)
 
-# for fwel cause 1
+# for fwel cause 1 ----
 coefs <- fw_coefs$fwelnet$cause1
 cause <- 1
 
@@ -69,18 +87,23 @@ test_cause <- test[, c("time", "status", names(coefs)), with = FALSE]
 csc_model <- CSC(formula = Hist(time, status) ~ ., data = train_cause, cause = cause)
 
 
-cbind(csc_model$models$`Cause 1`$coefficients, coefs) |> View()
+cbind(cox = csc_model$models$`Cause 1`$coefficients, fwelnet = coefs) |>
+  round(3 )
 
 # lp
 lp <- as.numeric(as.matrix(train_cause[, -c(1, 2)]) %*% coefs)
+range(lp)
 bfit <- survival::basehaz(csc_model$models$`Cause 1`)
 quantile(bfit$time, probs = seq(0.1, 0.5, .1))
 
 bfit |>
-  dplyr::filter(round(time, 4) == 0.7705)
+  dplyr::filter(time == median(time))
+
+plot(bfit$time, bfit$hazard)
 
 # surv prob
-exp(-0.1075885) ^ exp(lp) |> summary()
+#exp(-0.1075885 * exp(lp)) |> summary()
+exp(-0.087395 * exp(lp)) |> summary()
 
 # Get quantiles of time points from full dataset to allow later aggregation per timepoint
 eval_times <- quantile(c(train$time, test$time), probs = seq(0.1, 0.75, .05), names = FALSE)
@@ -89,6 +112,7 @@ eval_times <- quantile(c(train$time, test$time), probs = seq(0.1, 0.75, .05), na
 # Yikes
 # Estimated risk outside the range [0,1].
 # Possible cause: incorrect extrapolation, i.e., time and/or covariates used for the prediction differ from those used to fit the Cox models.
+model = "fwelnet"
 mod_scores_auc <- Score(
   list2(!!model := csc_model), # uses rlang splicing to get list(fwelnet = csc_1)
   #predictRisk.args = list(CauseSpecificCox = list(product.limit = FALSE)),
@@ -114,11 +138,87 @@ mod_scores_brier <- Score(
 )
 
 
-result <- mod_scores_auc$AUC$score[mod_scores_brier$Brier$score, on = c("model", "times")]
-result <- data.table::melt(result, id.vars = c("model", "times"),
+result_fwelnet <- mod_scores_auc$AUC$score[mod_scores_brier$Brier$score, on = c("model", "times")]
+result_fwelnet <- data.table::melt(result_fwelnet, id.vars = c("model", "times"),
                            value.name = "score", variable.name = "metric",
                            meausure.vars = c("AUC", "Brier", "IBS", "IPA"))
 
-result$cause <- cause
-result
+result_fwelnet$cause <- cause
+result_fwelnet
 
+# for glmnet cause 1 ----
+coefs <- fw_coefs$glmnet$cause1
+
+# sub-select data
+train_cause <- train[, c("time", "status", names(coefs)), with = FALSE]
+test_cause <- test[, c("time", "status", names(coefs)), with = FALSE]
+
+csc_model <- CSC(formula = Hist(time, status) ~ ., data = train_cause, cause = cause)
+
+
+cbind(cox = csc_model$models$`Cause 1`$coefficients, glmnet = coefs) |>
+  round(3 )
+
+# lp
+lp <- as.numeric(as.matrix(train_cause[, -c(1, 2)]) %*% coefs)
+range(lp)
+bfit <- survival::basehaz(csc_model$models$`Cause 1`)
+quantile(bfit$time, probs = seq(0.1, 0.5, .1))
+
+bfit |>
+  dplyr::filter(time == median(time))
+
+plot(bfit$time, bfit$hazard)
+
+# surv prob
+#exp(-0.1075885 * exp(lp)) |> summary()
+exp(-0.087395 * exp(lp)) |> summary()
+
+model = "glmnet"
+mod_scores_auc <- Score(
+  list2(!!model := csc_model), # uses rlang splicing to get list(fwelnet = csc_1)
+  #predictRisk.args = list(CauseSpecificCox = list(product.limit = FALSE)),
+  formula = Hist(time, status) ~ 1,
+  data = test_cause,
+  metrics = "AUC",
+  cause = cause,
+  se.fit = FALSE,
+  times = eval_times
+)
+
+
+mod_scores_brier <- Score(
+  list2(!!model := csc_model),
+  #predictRisk.args = list(CauseSpecificCox = list(product.limit = FALSE)),
+  formula = Hist(time, status) ~ 1,
+  data = test_cause,
+  metrics = c("Brier"),
+  summary = c("ibs", "ipa"),
+  cause = cause,
+  se.fit = FALSE,
+  times = eval_times
+)
+
+
+result_glmnet <- mod_scores_auc$AUC$score[mod_scores_brier$Brier$score, on = c("model", "times")]
+result_glmnet <- data.table::melt(result_glmnet, id.vars = c("model", "times"),
+                           value.name = "score", variable.name = "metric",
+                           meausure.vars = c("AUC", "Brier", "IBS", "IPA"))
+result_glmnet$cause <- cause
+
+# Aggregate + plot ----
+library(ggplot2)
+result <- rbind(result_fwelnet, result_glmnet)
+
+result |>
+  #dplyr::filter(metric == "Brier") |>
+  ggplot(aes(x = times, y = score, color = model, fill = model)) +
+  #facet_grid(cols = vars(model), rows = vars(metric), scales = "free") +
+  facet_grid(rows = vars(metric), scales = "free") +
+  geom_line() +
+  labs(
+    x = "Event Time (t)", y = "Metric",
+    color = NULL, fill = NULL
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(legend.position = "bottom")
