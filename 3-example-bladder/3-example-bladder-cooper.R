02b744e0ba1bdeba892f559f5a341b8684610255
Lukas Burk <lukas@quantenbrot.de>
2023-12-12 00:33:43 +0100

    20231212003343

--- 3-example-bladder/3-example-bladder-cooper.R
+++ 3-example-bladder/3-example-bladder-cooper.R
@@ -1,22 +1,17 @@
 set.seed(2023)
-
 bladder <- readRDS(here::here("data/bladder-binder-clinical_geno.rds"))
 
-table(bladder$status)
-
+# Fit CooPeR
 fit <- cooper::cooper(
-  bladder,
-  mt_max_iter = 3,
-  alpha = 1,
-  t = 100,
-  thresh = 1e-7,
-  stratify_by_status = TRUE,
-  nfolds = 5
+  bladder, mt_max_iter = 3,
+  alpha = 1, t = 100, thresh = 1e-7,
+  stratify_by_status = TRUE, nfolds = 5
 )
 
-# Extract
-glmnet_beta1 <- coef(fit, event = 1, use_initial_fit = TRUE)
-glmnet_beta2 <- coef(fit, event = 2, use_initial_fit = TRUE)
+# Extract coefficients from initial Coxnet
+coxnet_beta1 <- coef(fit, event = 1, use_initial_fit = TRUE)
+coxnet_beta2 <- coef(fit, event = 2, use_initial_fit = TRUE)
+# Extract final coefficients from CooPeR
 cooper_beta1 <- coef(fit, event = 1)
 cooper_beta2 <- coef(fit, event = 2)
 
@@ -25,16 +20,16 @@ selected <- list(
     cause1 = cooper_beta1[cooper_beta1 != 0],
     cause2 = cooper_beta2[cooper_beta2 != 0]
   ),
-  glmnet = list(
-    cause1 = glmnet_beta1[glmnet_beta1 != 0],
-    cause2 = glmnet_beta2[glmnet_beta2 != 0]
+  coxnet = list(
+    cause1 = coxnet_beta1[coxnet_beta1 != 0],
+    cause2 = coxnet_beta2[coxnet_beta2 != 0]
   )
 )
 
 # Covariables shared between causes for
-# cooper:
+# CooPeR:
 intersect(names(selected$cooper$cause1), names(selected$cooper$cause2))
-# glmnet:
-intersect(names(selected$glmnet$cause1), names(selected$glmnet$cause2))
+# Coxnet:
+intersect(names(selected$coxnet$cause1), names(selected$coxnet$cause2))
 
 
