07829bded93f85c4688788677604896de53098ec
Lukas Burk <lukas@quantenbrot.de>
2024-06-19 15:06:48 +0200

    Bladder ex redo

---
+++ 3-example-bladder/3-example-bladder-combined.R
@@ -0,0 +1,155 @@
+source(here::here("R/utils.R"))
+library(data.table)
+library(randomForestSRC)
+
+if (!dir.exists(here::here("results"))) dir.create(here::here("results"))
+
+set.seed(2023)
+bladder <- readRDS(here::here("data/bladder-binder-clinical_geno.rds"))
+bladder_dt <- data.table::as.data.table(bladder)
+
+# CooPeR --------------------------------------------------------------------------------------
+
+cooperfit <- cooper::cooper(
+  bladder, mt_max_iter = 3,
+  alpha = 1, t = 100, thresh = 1e-7,
+  stratify_by_status = TRUE, nfolds = 5
+)
+
+saveRDS(cooperfit, here::here("results/3-bladder-cooper.rds"))
+
+# Extract coefficients from initial Coxnet
+coxnet_beta1 <- coef(cooperfit, event = 1, use_initial_fit = TRUE)
+coxnet_beta2 <- coef(cooperfit, event = 2, use_initial_fit = TRUE)
+# Extract final coefficients from CooPeR
+cooper_beta1 <- coef(cooperfit, event = 1)
+cooper_beta2 <- coef(cooperfit, event = 2)
+
+selected <- list(
+  cooper = list(
+    cause1 = cooper_beta1[cooper_beta1 != 0],
+    cause2 = cooper_beta2[cooper_beta2 != 0]
+  ),
+  coxnet = list(
+    cause1 = coxnet_beta1[coxnet_beta1 != 0],
+    cause2 = coxnet_beta2[coxnet_beta2 != 0]
+  )
+)
+
+# Covariables shared between causes for
+# CooPeR:
+(cooper_shared <- intersect(names(selected$cooper$cause1), names(selected$cooper$cause2)))
+# Coxnet:
+(coxnet_shared <- intersect(names(selected$coxnet$cause1), names(selected$coxnet$cause2)))
+
+coxnet_beta1[names(coxnet_beta1) == "age"]
+coxnet_beta2[names(coxnet_beta2) == "age"]
+cooper_beta1[names(cooper_beta1) == "age"]
+cooper_beta2[names(cooper_beta2) == "age"]
+
+reference <- readxl::read_excel("data-raw/10780432ccr062940-sup-supplemental_file_2.xls", sheet = "Progression classifier probes") |>
+  janitor::clean_names()
+
+cooper_shared[cooper_shared %in% reference$probe_id]
+
+reference |>
+  dplyr::filter(probe_id %in% cooper_shared)
+
+
+
+# rfsrc ---------------------------------------------------------------------------------------
+
+set.seed(2023)
+
+rf_c1 <- rfsrc_tuned(
+  xdat = bladder_dt,
+  splitrule = "logrank",
+  importance = "random",
+  cause = 1
+)
+
+rf_c2 <- rfsrc_tuned(
+  xdat = bladder_dt,
+  splitrule = "logrank",
+  importance = "random",
+  cause = 2
+)
+
+saveRDS(rf_c1, here::here("results/3-bladder-rfsrc-c1.rds"))
+saveRDS(rf_c2, here::here("results/3-bladder-rfsrc-c2.rds"))
+
+vimps <- data.table::data.table(
+  variable = rownames(rf_c1[["importance"]]),
+  vi_c1 = rf_c1[["importance"]][, 1],
+  vi_c2 = rf_c2[["importance"]][, 2]
+)
+
+vimps[, vita_c1 := ifelse(vi_c1 <= abs(min(vi_c1)), 0, vi_c1)]
+vimps[, vita_c2 := ifelse(vi_c2 <= abs(min(vi_c2)), 0, vi_c2)]
+
+saveRDS(vimps, here::here("results/3-bladder-rfsrc-vimps.rds"))
+
+vimps[vita_c1 != 0 | vita_c2 != 0, ]
+
+# Count selected per cause
+nrow(vimps[vita_c1 != 0, ])
+nrow(vimps[vita_c2 != 0, ])
+
+# Overlap between causes?
+vimps[vita_c1 != 0 & vita_c2 != 0, ]
+
+
+# Coxboost ----------------------------------------------------------------
+cbfit <- coxboost_tuned(
+  xdat = bladder_dt,
+  cmprsk = "csh"
+)
+
+saveRDS(cbfit, here::here("results/3-bladder-coxboost.rds"))
+
+cb_coefs <- coef(cbfit)
+coefs_c1 <- cb_coefs[[1]][cb_coefs[[1]] != 0]
+coefs_c2 <- cb_coefs[[2]][cb_coefs[[2]] != 0]
+
+coefs_c1
+coefs_c2
+
+# Count selected per cause
+length(coefs_c1)
+length(coefs_c2)
+
+# Overlap between causes?
+intersect(names(coefs_c1), names(coefs_c2))
+
+
+# Performances --------------------------------------------------------------------------------
+
+rsfs = list(rf_c1, rf_c2)
+
+scores_cmb <- data.table::rbindlist(lapply(1:2, function(cause) {
+  eval_times_df <- eval_times_quant(bladder_dt, cause = cause)
+
+  rr_glmnet <- refit_glmnet(cooperfit, bladder_dt, event = cause, alpha = alpha)
+
+  scores = Score(
+    list(
+      cooper = cooperfit,
+      glmnet = rr_glmnet,
+      rsfrc = rsfs[[cause]],
+      coxboost = cbfit
+    ),
+    formula = Hist(time, status) ~ 1,
+    data = bladder_dt,
+    metrics = c("Brier", "AUC"),
+    summary = c("ibs", "ipa"),
+    se.fit = FALSE,
+    cause = cause,
+    times = eval_times_df$times
+  )
+
+  scores_dat <- cleanup_score(scores)
+  scores_dat$cause = cause
+  merge(scores_dat, eval_times_df, by = "times")
+}))
+
+saveRDS(scores_cmb, here::here("results/3-bladder-scores.rds"))
